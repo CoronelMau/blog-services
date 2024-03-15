4bbe317f7313b386a22c24188ca8ba75178ec17f
unknown <8mau.coronel@gmail.com>
2024-03-15 13:50:27 -0600

    Returning posts with comments, likes, and date; in user posts and follow posts controller

--- controllers/follow-posts.controller.js
+++ controllers/follow-posts.controller.js
@@ -1,6 +1,8 @@
 import UserSchema from '../Schemas/user.schema.js';
 import PostSchema from '../schemas/post.schema.js';
 import FollowerSchema from '../schemas/follower.schema.js';
+import CommentSchema from '../schemas/comment.schema.js';
+import LikeSchema from '../schemas/like.schema.js';
 
 export default async function getFollowPostController(req, res) {
   const { id } = req;
@@ -19,46 +21,95 @@ export default async function getFollowPostController(req, res) {
       msg: `User with id ${id} does not follow nobody`,
     });
 
-  const followingIds = follows.map((follow) => follow.following_id);
+  const followPosts = await Promise.all(
+    follows.map(async (follow) => {
+      const posts = await PostSchema.findAll({
+        where: { user_id: follow.following_id },
+      });
 
-  const postsByFollowing = await PostSchema.findAll({
-    where: { user_id: followingIds },
-  });
-  if (!postsByFollowing)
-    return res.status(401).json({
-      msg: `User with id ${followingIds.following_id} does not have posts`,
-    });
+      const userPoster = await UserSchema.findOne({
+        where: { id: follow.following_id },
+      });
 
-  const sortedPostFollowers = postsByFollowing.sort(
-    (a, b) => b.createdAt - a.createdAt
+      const postsWithDetails = await Promise.all(
+        posts.map(async (post) => {
+          const comments = await CommentSchema.findAll({
+            where: { post_id: post.id },
+          });
+
+          const commentsData = await Promise.all(
+            comments.map(async (comment) => {
+              const userComment = await UserSchema.findOne({
+                where: { id: comment.user_id },
+              });
+
+              return {
+                user: userComment.username,
+                content: comment.content,
+              };
+            })
+          );
+
+          const likesCount = await LikeSchema.count({
+            where: { post_id: post.id },
+          });
+
+          return {
+            id: post.id,
+            user: userPoster.username,
+            url: post.url,
+            content: post.text,
+            comments: commentsData,
+            likes: likesCount,
+            date: post.createdAt,
+          };
+        })
+      );
+
+      return postsWithDetails;
+    })
   );
-
-  const posterNames = await UserSchema.findAll({
-    where: { id: followingIds },
-  });
 
   const userPosts = await PostSchema.findAll({ where: { user_id: id } });
 
-  const sortedUserPosts = userPosts.sort((a, b) => b.createdAt - a.createdAt);
+  const userPostsDetails = await Promise.all(
+    userPosts.map(async (post) => {
+      const comments = await CommentSchema.findAll({
+        where: { post_id: post.id },
+      });
 
-  const usernames = posterNames.map((poster) => poster.username);
+      const commentsData = await Promise.all(
+        comments.map(async (comment) => {
+          const userComment = await UserSchema.findOne({
+            where: { id: comment.user_id },
+          });
 
-  const postInfo = sortedPostFollowers.map((post) => {
-    return {
-      user: usernames[followingIds.indexOf(post.user_id)],
-      postId: post.id,
-      text: post.text,
-    };
-  });
+          return {
+            user: userComment.username,
+            content: comment.content,
+          };
+        })
+      );
+      const likesCount = await LikeSchema.count({
+        where: { post_id: post.id },
+      });
 
-  const allPosts = [
-    ...postInfo,
-    ...sortedUserPosts.map((post) => ({
-      user: user.username,
-      postId: post.id,
-      text: post.text,
-    })),
-  ];
+      return {
+        id: post.id,
+        url: post.url,
+        content: post.text,
+        comments: commentsData,
+        likes: likesCount,
+        date: post.createdAt,
+      };
+    })
+  );
 
-  return res.status(200).json(allPosts);
+  const finalPosts = followPosts
+    .reduce((acc, curr) => acc.concat(curr), [])
+    .concat(userPostsDetails);
+
+  finalPosts.sort((a, b) => b.date - a.date);
+
+  return res.status(200).json({ finalPosts });
 }
